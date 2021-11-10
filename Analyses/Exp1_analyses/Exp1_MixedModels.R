306f28447699365c76e384e730b8dcf5ba21b7f0
Stef Herregods <herregodsstef@hotmail.com>
2021-11-10 10:20:09 +0100

    Outline mixed models exp1

---
+++ Analyses/Exp1_analyses/Exp1_MixedModels.R
@@ -0,0 +1,27 @@
+# Internship project: ConfidenceBounds (2021-2022)
+# Script contains the mixed models code used to check hypothesis (a), (b) and 
+# (c) of experiment 1.
+
+
+# Activating packages
+
+library(lme4)
+
+# Loading data
+
+
+# Hypothesis (a)
+# Faster decision reaction times and lower accuracy when participants are 
+# asked to respond quickly (vs accurately), 
+# without an effect on confidence reaction times and confidence ratings.
+
+
+# (b) Faster confidence reaction times and less accurate confidence ratings 
+# (i.e., confidence ratings that are less precise in the prediction of accuracy)
+# when participants are asked to give quick confidence ratings 
+# (vs think carefully about their ratings), 
+# with no effect on decision accuracy and decision reaction times.
+
+
+# (c) Faster and more accurate decisions, and faster and more accurate 
+# confidence ratings in easier trials than in more difficult trials.

188840e57697d6c87c3b6c30c67f57da6ca1af89
Stef Herregods <60329530+StefHerregods@users.noreply.github.com>
2021-11-29 13:51:05 +0100

    Save data option

--- DataChecks/Exp1_MergeData.R
+++ DataChecks/Exp1_MergeData.R
@@ -4,7 +4,7 @@
 
 # Setting working directory
 
-setwd("C:\\Users\\herre\\OneDrive\\Bureaublad\\Internship\\Results\\Exp1_results")
+setwd("C:\\Users\\herre\\OneDrive\\Bureaublad\\Internship\\Results\\Exp1_Results")
 
 # Initialize data_full
 
@@ -22,11 +22,15 @@ for (j in 1:9){
     if (file.exists(file_name)){
       
       data_temp <- read.csv(file=file_name)
-      data_temp$subject <- i  # Necessary?
+      data_temp$subject <- i
       data_temp$batch <- j
       data_full <- rbind(data_temp,data_full)
       
     }
   }
 }
+
+# Save full data set
+
+write.csv(data_full,"Exp1_data_full.csv")
 

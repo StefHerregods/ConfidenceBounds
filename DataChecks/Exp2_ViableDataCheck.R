6340a3288c2a1ae0cc29221740a59552109412c6
Stef Herregods <herregodsstef@hotmail.com>
2021-11-30 10:27:06 +0100

    Finish check 1

--- DataChecks/Exp2_ViableDataCheck.R
+++ DataChecks/Exp2_ViableDataCheck.R
@@ -1,13 +1,12 @@
 # November 2021
-# Script checks for (a) viability of the data (as described in preregistration experiment 2) 
-# and (b) irregularities in data of experiment 2
+# Script checks for (a) viability of the data (as described in preregistration 
+# experiment 2) and (b) irregularities in data of experiment 2
 
 
 
 # Variables
 
 alpha <- 0.05  # Significance level
-batches <- 10  # change to amount of batches Exp 2
 
 # Setting working directory
 
@@ -30,22 +29,31 @@ data_full <- read.csv(file="Exp2_data_full.csv")
 colnames(data_full)[1] <- gsub('^...','',colnames(data_full)[1])
 
 # Add data checks
+
 data_full$check1 <- FALSE
 data_full$check2 <- FALSE
 data_full$check3 <- FALSE
 data_full$check4 <- FALSE
 
 # Loop through participants
-for (i in 1:40){
-  for (j in 1:batches){
-    if (length(data_full$batch[data_full$batch == j & data_full$subject == i]) > 0){
+
+for (i in unique(data_full$sub)){
+  for (j in unique(data_full$batch)){
+
+    # Check 1: Finishing the experiment
+
+    final_block <- suppressWarnings(max(subset(data_full, data_full$batch == j 
+                                               & data_full$sub == i, 
+                                               select = block)))
+    final_trial <- suppressWarnings(max(subset(data_full, data_full$batch == j 
+                                               & data_full$sub == i & 
+                                                 data_full$block == final_block, 
+                                               select = withinblocktrial)))
+    if (final_block == 15 & final_trial == 59){
+      data_full$check1[data_full$batch == j & data_full$sub == i] <- TRUE  
+
       
-      # Check 1: Finishing the experiment
-      final_block <- max(data_full$block[data_full$batch == j & data_full$subject == i])
-      final_trial <- max(data_full$withinblocktrial[data_full$batch == j & data_full$subject == i & data_full$block == 15])
-      if (final_block == 15 & final_trial == 59){
-        data_full$check1[data_full$batch == j & data_full$subject == i] <- TRUE  
-      }
+    
       
     }
   }
@@ -53,12 +61,7 @@ for (i in 1:40){
 
 
 
-    final_block <- max(data_temp$block)
-    if (final_block == 15){
-      final_trial <- max(data_temp$ï..withinblocktrial[data_temp$block == 15])
-      if (final_trial == 59){
-        data_temp$check1 <- TRUE
-        
+
         # Step 2: Required more than 7 training blocks
         
         training_blocks <- max(data_temp$block_repetition[data_temp$block == 1]) + max(data_temp$block_repetition[data_temp$block == 2]) + max(data_temp$block_repetition[data_temp$block == 3])

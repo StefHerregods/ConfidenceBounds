ed24bc30a9db792fccfd5e0da15ce9cbd84efdb4
Stef Herregods <herregodsstef@hotmail.com>
2022-02-01 10:11:21 +0100

    Reg 1 finished

--- Analyses/Exp1_analyses/Exp1_MixedModels.R
+++ Analyses/Exp1_analyses/Exp1_MixedModels.R
@@ -2,38 +2,39 @@
 # Script contains the mixed effect models code used to check hypothesis (a), (b) and 
 # (c) of experiment 1.
 
-# Questions:
-# normalize RT? log of rt?
-# REML = FALSE or TRUE
-# logistic for binary outcome variables
-
 
 # Activating packages
 
 library(lme4)
-library(lmerTest)
 library(dplyr)
 library(ggplot2)
 library(grid)
 library(car)
+library(effects)
 
 # Setting working directory
 
 setwd('C:\\Users\\herre\\Desktop\\Internship\\Results\\Exp1_Results')
 
-# Loading data
-
 df <- read.csv(file = "Exp1_data_viable.csv")
+
+# Transform variables into factors
+
+df$rt_manipulation <- ifelse(df$manipulation %in% c("AccAcc", "AccFast"), 1, 0)
+df$rt_manipulation <- as.factor(df$rt_manipulation)
+df$rtconf_manipulation <- ifelse(df$manipulation %in% c("AccAcc", "FastAcc"), 1, 0)
+df$rtconf_manipulation <- as.factor(df$rtconf_manipulation)
+df$coherence <- as.factor(df$coherence)
 
 
 ### Effect of manipulations on decision RT ###
 
 
-# Only use data with correct decisions
+# Based on correct data only
 
 df_correct <- subset(df, cor == 1)
 
-# Are random slopes necessary?
+# Are random slopes necessary? -> Yes (but less for coherence)
 
 plot1 <- ggplot(df_correct, aes(x = as.factor(rt_manipulation), y = rt, group = sub)) +
   stat_smooth(geom='line', alpha=1, se=FALSE, method = 'lm') +
@@ -42,7 +43,7 @@ plot2 <- ggplot(df_correct, aes(x = as.factor(rtconf_manipulation), y = rt, grou
   stat_smooth(geom='line', alpha=1, se=FALSE, method = 'lm') +
   theme_minimal()
 plot3 <- ggplot(df_correct, aes(x = coherence, y = rt, group = sub)) +
-  stat_smooth(geom='line', alpha=1, se=FALSE, method = 'lm') +
+  stat_smooth(geom='line', alpha=1, se=FALSE) +
   theme_minimal()
 
 grid.newpage()
@@ -51,98 +52,109 @@ vplayout <- function(x,y){
   viewport(layout.pos.row=x, layout.pos.col=y)}
 print(plot1, vp=vplayout(1,1)); print(plot2, vp=vplayout(1,2)); print(plot3, vp=vplayout(1,3))
 
-
 # Model comparisons (through likelihood ratio tests)
 
 RT_1 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), data = df_correct)
 RT_2 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation|sub), data = df_correct)
 RT_3 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rtconf_manipulation|sub), data = df_correct)
-RT_4 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), data = df_correct)
+RT_4 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), data = df_correct)  # Fails to converge
 
-anova(RT_1, RT_2) # significant
-anova(RT_1, RT_3) # significant
-anova(RT_1, RT_4) # significant
+anova(RT_1, RT_2)  # Significant
+anova(RT_1, RT_3)  # Significant
 
 RT_5 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation|sub), data = df_correct)
 
-anova(RT_2, RT_5) # significant
-anova(RT_3, RT_5) # significant
-
-RT_6 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rtconf_manipulation + coherence|sub), data = df_correct)
-
-anova(RT_3, RT_6) # significant
-anova(RT_4, RT_6) # significant
-
-RT_7 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence + rt_manipulation|sub), data = df_correct)
-
-anova(RT_4, RT_7) # significant
-anova(RT_2, RT_7) # significant
-
-RT_8 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence + rt_manipulation + rtconf_manipulation|sub), data = df_correct)
-
-anova(RT_5, RT_8) # significant
-anova(RT_5, RT_8) # significant
-anova(RT_5, RT_8) # significant
+anova(RT_2, RT_5)  # Significant
+anova(RT_3, RT_5)  # Significant
 
 
-# Any model including all 3 random slopes + random slope for an interaction fails to converge
-# The model below converges, but does not include coherence
-RT_9 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation * rtconf_manipulation|sub), data = df_correct)
+RT_6 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation * rtconf_manipulation|sub), data = df_correct)
 
-anova(RT_8, RT_9)   # Equally complex model -> no p-value (0 degrees of freedom)
-                    # RT_8 better AIC (lower), BIC (lower), log-likelihood (higher)
+anova(RT_5, RT_6)  # Significant
 
-# Model assumptions 
+# Model assumptions -> violated -> log transform rt
 
-plot(RT_8)
-RT_8_resid <- resid(RT_8)
-RT_8_fit <- fitted(RT_8)
-qqnorm(RT_8_resid)
-qqline(RT_8_resid)
-df_temp <- data.frame(cbind(RT_8_fit, RT_8_resid))
-ggplot(df_temp, aes(x = RT_8_fit, y = RT_8_resid)) + geom_point() + geom_smooth(se = F) + geom_hline(aes(yintercept=0))
-# VIF
-
-library(effects)
-plot(effect('rt_manipulation:coherence',RT_8))
-data.frame(effect('rt_manipulation:coherence',RT_8))
-fixef(RT_8)
-ranef(RT_8)
-
+plot(RT_6)
+RT_6_resid <- resid(RT_6)
+RT_6_fit <- fitted(RT_6)
+qqnorm(RT_6_resid)
+qqline(RT_6_resid)
+df_temp <- data.frame(cbind(RT_6_fit, RT_6_resid))
+ggplot(df_temp, aes(x = RT_6_fit, y = RT_6_resid)) + geom_point() + geom_smooth(se = F) + geom_hline(aes(yintercept=0))
 
 # Log transform
 
 df_correct$rt_log <- log(df_correct$rt)
 
-RT_10 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence + rt_manipulation + rtconf_manipulation|sub), data = df_correct)
+df_correct$rt_log <- scale(df_correct$rt_log, scale = FALSE) 
 
-plot(RT_10)
-RT_10_resid <- resid(RT_10)
-RT_10_fit <- fitted(RT_10)
-qqnorm(RT_10_resid)
-qqline(RT_10_resid)
-df_temp <- data.frame(cbind(RT_10_fit, RT_10_resid))
-ggplot(df_temp, aes(x = RT_10_fit, y = RT_10_resid)) + geom_point() + geom_smooth(se = F) + geom_hline(aes(yintercept=0))
+# Model selection with log transformed rt
 
+RT_7 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), data = df_correct)
+RT_8 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation|sub), data = df_correct)
+RT_9 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rtconf_manipulation|sub), data = df_correct)
+RT_10 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), data = df_correct)  # Singular fit
 
+anova(RT_7, RT_8)  # Significant
+anova(RT_7, RT_9)  # Significant
 
+RT_11 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation|sub), data = df_correct)
 
+anova(RT_8, RT_11)  # Significant
+anova(RT_9, RT_11)  # Significant
 
+RT_12 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation * rtconf_manipulation|sub), data = df_correct)
 
+anova(RT_11, RT_12)  # Significant
 
-Anova(RT_10) # binary variables (look for literature)
-anova(RT_1) # continuous variables (look for literature)
-anova(RT_base, RT_1)
+# Model assumptions
 
-shapiro.test(RT_2)
+plot(RT_12)
+RT_12_resid <- resid(RT_12)
+RT_12_fit <- fitted(RT_12)
+qqnorm(RT_12_resid)
+qqline(RT_12_resid)
+df_temp <- data.frame(cbind(RT_12_fit, RT_12_resid))
+ggplot(df_temp, aes(x = RT_12_fit, y = RT_12_resid)) + geom_point() + geom_smooth(se = F) + geom_hline(aes(yintercept=0))
 
+# Multicollinearity - VIF
+
+# https://stackoverflow.com/questions/26633483/collinearity-after-accounting-for-random-mixed-effects
+vif.lme <- function (fit) {  
+  ## adapted from rms::vif
+  v <- vcov(fit)
+  nam <- names(fixef(fit))
+  ## exclude intercepts
+  ns <- sum(1 * (nam == "Intercept" | nam == "(Intercept)"))
+  if (ns > 0) {
+    v <- v[-(1:ns), -(1:ns), drop = FALSE]
+    nam <- nam[-(1:ns)] }
+  d <- diag(v)^0.5
+  v <- diag(solve(v/(d %o% d)))
+  names(v) <- nam
+  v }
+
+vif.lme(RT_12)
+
+# Model interpretation
+
+anova(RT_12)
+confint(RT_12)
+
+plot(effect('rt_manipulation',RT_12))
+plot(effect('coherence',RT_12))
+plot(effect('rt_manipulation:coherence',RT_12))
+plot(effect('rt_manipulation:rtconf_manipulation',RT_12))
+
+data.frame(effect('rt_manipulation:coherence',RT_12))
+fixef(RT_12)
+ranef(RT_9)
 
 
 ### Effect of manipulations on confidence rating RT ###
 
-# Which data set to use?
 
-# Are random slopes necessary? !!!based on correct data only?!
+# Are random slopes necessary? 
 
 plot1 <- ggplot(df_correct, aes(x = as.factor(rt_manipulation), y = rtconf, group = sub)) +
   stat_smooth(geom='line', alpha=1, se=FALSE, method = 'lm') +
@@ -162,26 +174,46 @@ print(plot1, vp=vplayout(1,1))
 print(plot2, vp=vplayout(1,2))
 print(plot3, vp=vplayout(1,3))
 
+# Model comparisons (through likelihood ratio tests)
 
-### Effect of manipulations on ??? ###
+RTconf_1 <- lmer(rtconf ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), data = df_correct)
+RTconf_2 <- lmer(rtconf ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation|sub), data = df_correct)
+RTconf_3 <- lmer(rtconf ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rtconf_manipulation|sub), data = df_correct)
+RTconf_4 <- lmer(rtconf ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), data = df_correct)  # Fails to converge
 
-
-
-
-
-
-
-RT_2 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), data = df)
-
-RT_base <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation + coherence|sub), data = df)
-RT_base <- lmer(rt.log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation + coherence|sub), data = df)
-RT_base <- lmer(rt.scaled ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation + coherence|sub), data = df)
-
-RT_base <- glm(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation * rtconf_manipulation * coherence|sub), family=Gamma	(link = "inverse"), data=df)
+anova(RT_1, RT_2)  # Significant
+anova(RT_1, RT_3)  # Significant
 
 
-df$cor.factor <- as.factor(df$cor)
-RT_base <- glm(cor.factor ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation + coherence|sub), family = binomial	(link = "logit"), data = df)
+
+# Model assumptions
+
+plot(RTconf_12)
+RTconf_12_resid <- resid(RTconf_12)
+RTconf_12_fit <- fitted(RTconf_12)
+qqnorm(RTconf_12_resid)
+qqline(RTconf_12_resid)
+df_temp <- data.frame(cbind(RTconf_12_fit, RTconf_12_resid))
+ggplot(df_temp, aes(x = RTconf_12_fit, y = RTconf_12_resid)) + geom_point() + geom_smooth(se = F) + geom_hline(aes(yintercept=0))
+
+
+
+
+
+
+
+
+
+
+
+
+### Effect of manipulations on accuracy ###
+
+
+
+
+
+
 
 
 
@@ -209,28 +241,7 @@ df$rt.scaled <- scale(df$rt)
 
 
 
-# Effect on decision reaction time
 
-modelA_1 <- lmer(rt ~ 1 + (1|sub), REML = FALSE, data = df)  # Intercept only model
-summary(modelA_1)
-confint(modelA_1)
-
-modelA_2 <- lmer(rt ~ 1 + rt_manipulation + rtconf_manipulation + coherence + (1|sub), REML = FALSE, data = df)  # Adding manipulations
-summary(modelA_2)
-confint(modelA_2)
-
-anova(modelA_1, modelA_2)
-
-modelA_3 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation  * coherence + (1|sub), REML = FALSE, data = df)  # Adding manipulations
-summary(modelA_3)
-confint(modelA_3)
-anova(modelA_3)
-
-anova(modelA_2, modelA_3)
-
-modelA_4 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation + rt_manipulation * coherence + (1|sub), REML = FALSE, data = df)  # Adding manipulations
-summary(modelA_4)
-confint(modelA_4)
 
 #anova(modelA_3, modelA_4)
 
@@ -257,39 +268,4 @@ ggplot(df_temp, aes(x = modelA_4_fit, y = modelA_4_resid)) + geom_point() + geom
 # with no effect on decision accuracy and decision reaction times.
 
 
-# Exploration
 
-# Random slopes necessary?
-df %>%
-  ggplot(aes(x = rtconf_manipulation, y = rtconf)) +
-  geom_point() +
-  geom_point(stat = "summary", fun = "mean", colour = 'red', size = 3) +
-  facet_wrap(vars(sub))
-
-# Effect on confidence rating reaction time
-
-modelB_1 <- lmer(rtconf ~ 1 + (1|sub), REML = FALSE, data = df)  # Intercept only model
-summary(modelB_1)
-confint(modelB_1)
-
-modelB_2 <- lmer(rtconf ~ 1 + rt_manipulation + rtconf_manipulation + coherence + (1|sub), REML = FALSE, data = df)  # Adding manipulations
-summary(modelB_2)
-confint(modelB_2)
-
-anova(modelB_1, modelB_2)
-
-modelB_3 <- lmer(rtconf ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), REML = FALSE, data = df)  # Adding manipulations
-summary(modelB_3)
-confint(modelB_3)
-
-anova(modelB_2, modelB_3)
-
-modelB_4 <- lmer(rtconf ~ 1 + rt_manipulation + rtconf_manipulation + (1 + rt_manipulation + rtconf_manipulation|sub), REML = FALSE, data = df)  # Adding manipulations
-summary(modelB_4)
-confint(modelB_4)
-
-anova(modelB_2, modelB_4)
-
-
-# (c) Faster and more accurate decisions, and faster and more accurate 
-# confidence ratings in easier trials than in more difficult trials.

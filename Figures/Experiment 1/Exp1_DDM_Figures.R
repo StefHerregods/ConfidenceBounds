c4ec9ca4abd003342c9f16aa4a7b1b26f1640683
Stef Herregods <herregodsstef@hotmail.com>
2022-03-08 17:33:42 +0100

    figures + anova's

--- Figures/Experiment 1/Exp1_DDM_Figures.R
+++ Figures/Experiment 1/Exp1_DDM_Figures.R
@@ -9,6 +9,7 @@ library(DEoptim)  # Optimization algorithm
 library(RcppZiggurat)  # Random number generator (normal distribution)
 library(ggpubr)
 library(gridExtra)
+library(rstatix)
 
 # Give R access to the DDM simulation function in C++
 
@@ -41,26 +42,40 @@ df[3:11] <- lapply(df[3:11], as.numeric)
 # v1
 
 ggplot(df, aes(x = manipulation, y = v1)) +
-  geom_point() +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 5, shape = 16) +
   geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = v1, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
+  stat_summary(aes(y = v1, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
   
 # v2
 
 ggplot(df, aes(x = manipulation, y = v2)) +
-  geom_point() +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 5, shape = 16) +
   geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = v2, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
-
+  stat_summary(aes(y = v2, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
 # v3
 
 ggplot(df, aes(x = manipulation, y = v3)) +
-  geom_point() +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 5, shape = 16) +
   geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = v3, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
+  stat_summary(aes(y = v3, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
 
 # v comparison
 
@@ -73,6 +88,31 @@ v_mean <- df_temp %>%
 ggplot(v_mean, aes(x = manipulation, y = value)) +
   geom_line(aes(group = name, colour = name), lty = 5, plt = 5, size = 1) +
   geom_point() 
+
+###
+df2 <- pivot_longer(data = df, cols = c(v1, v2, v3), names_to = 'v_var', values_to = 'v')
+
+res.aov <- anova_test(data = df2, dv = v, wid = sub, within = c(v_var, manipulation))
+res.aov
+
+pwc <- df2 %>%
+  group_by(v_var) %>%
+  pairwise_t_test(
+    v ~ manipulation, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+data.frame(pwc)
+
+pwc <- df2 %>%
+  group_by(manipulation) %>%
+  pairwise_t_test(
+    v ~ v_var, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+data.frame(pwc)
+
+
+
 
   
 ggplot(df, aes(x = manipulation)) +
@@ -90,51 +130,175 @@ ggplot(df, aes(x = manipulation)) +
 # a
 
 ggplot(df, aes(x = manipulation, y = a)) +
-  geom_point() +
-  geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = a, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
+  #geom_violin(outlier.shape = NA, coef = 0) +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 5, shape = 16) +
+  geom_line(aes(group = sub), alpha = 0.3) +
+  stat_summary(aes(y = a, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+###
+
+res.aov <- anova_test(data = df, dv = a, wid = sub, within = manipulation)
+res.aov
+
+pwc <- df %>%
+  pairwise_t_test(
+    a ~ manipulation, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+data.frame(pwc)
+
+
+
+
+
+library(afex)
+#Model Specification
+SCR_ANOVA <- aov_car(a ~ manipulation + Error(sub/manipulation), data = df)
+SCR_ANOVA_summary <- summary(SCR_ANOVA)
+SCR_ANOVA_summary #Significant main effect of Phase
+
+##Contrasts (based on the model with outliers): As the F-test of the model with outliers is insignificant for the interaction, contrasts are expected to be insignificant
+###Setting up a reference grid
+library(lsmeans)
+ref_SCR <- lsmeans(SCR_ANOVA, c("manipulation"))
+ref_SCR
+
+###Specifying the contrasts as a list on the reference grid. Note that we test 2 pre-planned contrasts in which we compare the general SCR pattern between both groups, once for the first two phases and once for the last two phases.
+list_of_contrasts_SCR <- list(contrast1_SCR  = c(1, 0, 0, -1),
+                              contrast2_SCR  = c(0, 1, -1, 0), 
+                              contrast3_SCR  = c(0.5, -0.5, -0.5, 0.5)) #We compare the general SCR pattern between both groups in the acquisition and extinction phase. A positive score indicates higher values in the experimental group as expected.
+
+###Test whether the contrasts are significant. Note that as we have multiple pre-planned contrasts, we have to apply the Bonferroni-Holm correction to avoid inflating the type I error.
+Planned_contrasts_SCR <- summary(contrast(ref_SCR, list_of_contrasts_SCR), adjust = "holm") 
+Planned_contrasts_SCR #As expected, the contrasts are insignificant. However, the absolute value of the contrast suggests that the SCR is higher in the experimental group, particularly in the acquisition phase.
+
 
 # a2
 
 ggplot(df, aes(x = manipulation, y = a2)) +
-  geom_point() +
-  geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = a2, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
+  #geom_boxplot(outlier.shape = NA, coef = 0) +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 5, shape = 16) +
+  geom_line(aes(group = sub), alpha = 0.3) +
+  stat_summary(aes(y = a2, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+###
+
+pwc <- df %>%
+  pairwise_t_test(
+    a2 ~ manipulation, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+data.frame(pwc)
 
 # ter
 
 ggplot(df, aes(x = manipulation, y = ter)) +
-  geom_point() +
-  geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = ter, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
+  #geom_boxplot(outlier.shape = NA, coef = 0) +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 5, shape = 16) +
+  geom_line(aes(group = sub), alpha = 0.3) +
+  stat_summary(aes(y = ter, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) +
+  ylab("non-decision time")
+
+res.aov <- anova_test(data = df, dv = ter, wid = sub, within = manipulation)
+res.aov # significant
+
+
+pwc <- df %>%
+  pairwise_t_test(
+    ter ~ manipulation, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+data.frame(pwc)
 
 # postdriftmod
 
 ggplot(df, aes(x = manipulation, y = postdriftmod)) +
-  geom_point() +
-  geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = postdriftmod, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
+  #geom_boxplot(outlier.shape = NA, coef = 0) +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 5, shape = 16) +
+  geom_line(aes(group = sub), alpha = 0.3) +
+  stat_summary(aes(y = postdriftmod, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) +
+  ylab("postdriftmod")
+
+res.aov <- anova_test(data = df, dv = postdriftmod, wid = sub, within = manipulation)
+res.aov # not significant
+
+pwc <- df %>%
+  pairwise_t_test(
+    postdriftmod ~ manipulation, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+data.frame(pwc)
 
 # a2_slope
 
 ggplot(df, aes(x = manipulation, y = a2_slope)) +
-  geom_point() +
-  geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = a2_slope, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
+  #geom_boxplot(outlier.shape = NA, coef = 0) +
+  ylim(c(0,5)) +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 2, shape = 16) +
+  geom_line(aes(group = sub), alpha = 0.3) +
+  stat_summary(aes(y = a2_slope, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) +
+  ylab("urgency")
+
+res.aov <- anova_test(data = df, dv = a2_slope, wid = sub, within = manipulation)
+res.aov # not significant
+
+
+
 
 # ter2
 
 ggplot(df, aes(x = manipulation, y = ter2)) +
-  geom_point() +
-  geom_line(aes(group = sub), alpha = 0.2) +
-  stat_summary(aes(y = ter2, group = 1), fun = mean, colour="Blue", size = 4, shape = 95) +
-  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) 
+  #geom_boxplot(outlier.shape = NA, coef = 0) +
+  geom_point(colour = "#4D5382", alpha = 0.5, size = 5, shape = 16) +
+  geom_line(aes(group = sub), alpha = 0.3) +
+  stat_summary(aes(y = ter2, group = 1), fun = mean, colour="#4D5382", size = 4, shape = 95) +
+  scale_x_discrete(labels = c("Accurate decision\nAccurate confidence rating", "Accurate decision\nFast confidence rating", "Fast decision\nAccurate confidence rating", "Fast decision\nFast confidence rating")) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) +
+  ylab("non-decision time")
 
+res.aov <- anova_test(data = df, dv = ter2, wid = sub, within = manipulation)
+res.aov # not significant
+
+pwc <- df %>%
+  pairwise_t_test(
+    ter2 ~ manipulation, paired = TRUE,
+    p.adjust.method = "bonferroni"
+  )
+data.frame(pwc)
 
 
 

05902fc0e595bf107cb74906c2c70ac0e2b15578
Stef Herregods <herregodsstef@hotmail.com>
2022-04-24 12:16:44 +0200

    Update Exp2_DDM_ParameterEstimation_separated_2.R

--- Analyses/Exp2_analyses/Exp2_DDM_ParameterEstimation_separated_2.R
+++ Analyses/Exp2_analyses/Exp2_DDM_ParameterEstimation_separated_2.R
@@ -23,14 +23,14 @@ sourceCpp("C:\\Users\\herre\\OneDrive\\Documenten\\GitHub\\ConfidenceBounds\\Ana
 
 # Variable settings
 
-overwrite <- T  # Overwrite already existing files?
+overwrite <- F  # Overwrite already existing files?
 
 z <- 0.5  # Starting point (accuracy-coded dataset -> 0.5)
-ntrials <- 100  # Number of decision-making simulations per observation
+ntrials <- 1000  # Number of decision-making simulations per observation
 sigma <- 1  # Within-trial noise
-dt <- 0.01  # Precision
+dt <- 0.001  # Precision
 
-itermax <- 300  # Number of DeOptim iterations
+itermax <- 1000  # Number of DeOptim iterations
 
 # Variable vectors
 
@@ -390,7 +390,7 @@ for(i in 1:N){  # For each participant separately
     
     # Load existing individual results if these already exist
     
-    file_name <- paste0('Parameter_estimation_test\\new_results_sub_', i, '_', condLab[c], '.Rdata')
+    file_name <- paste0('Parameter_estimation_separated_2\\exp2_separated_2_results_sub_', i, '_', condLab[c], '.Rdata')
     if (overwrite == F & file.exists(file_name)){
 
       load(file_name)
@@ -403,8 +403,8 @@ for(i in 1:N){  # For each participant separately
       
       optimal_params <- DEoptim(chi_square_optim,  # Function to optimize
                                 # Possible values for v (for each level of coherence: 0.1, 0.2 and 0.4), a, ter, a2_upper, a2_lower, postdriftmod, a2_slope_upper, a2_slope_lower, ter2
-                                lower = c(0, 0, 0, .5,   0, 0.0001, 0.0001, 0,  0.0001,  0.0001, -2),  
-                                upper = c(3, 3, 3,  4, 1.5,     10,     10, 15, 10,      10,      2),
+                                lower = c(0, 0, 0, .5,   0, 0.0001, 0.0001, 0,  0.0001,  0.0001, -4),  
+                                upper = c(3, 3, 3,  4, 1.5,     15,     15, 15, 15,      15,      4),
                                 all_observations = tempDat, returnFit = 1, control = c(itermax = itermax))
       
       results <- summary(optimal_params)

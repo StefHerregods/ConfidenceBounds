7a944b79fea54010ab42843b53e2e581a68007ae
Stef Herregods <herregodsstef@hotmail.com>
2022-04-27 10:40:46 +0200

    structure change

--- Analyses/Exp1_analyses/Exp1_MixedModels.R
+++ Analyses/Exp1_analyses/Exp1_MixedModels.R
@@ -1,8 +1,11 @@
 # Internship project: ConfidenceBounds (2021-2022)
 # Script contains the mixed effect models code used to analyze RT's, confidence RT's and accuracy
 
+# Set-up ----
 
-# Activating packages
+rm(list = ls())
+
+## Activating packages 
 
 library(lme4)
 library(dplyr)
@@ -16,7 +19,7 @@ library(ggResidpanel)
 library(arm)
 library(splines)
 
-# VIF function
+## VIF function
 
 # https://stackoverflow.com/questions/26633483/collinearity-after-accounting-for-random-mixed-effects
 vif.lme <- function (fit) {  
@@ -33,13 +36,13 @@ vif.lme <- function (fit) {
   names(v) <- nam
   v }
 
-# Setting working directory
+## Setting working directory
 
 setwd('C:\\Users\\herre\\Desktop\\Internship\\Results\\Exp1_Results')
 
 df <- read.csv(file = "Exp1_data_viable.csv")
 
-# Transform variables into factors
+## Transform variables into factors
 
 df$rt_manipulation <- ifelse(df$manipulation %in% c("AccAcc", "AccFast"), 1, 0)
 df$rt_manipulation <- as.factor(df$rt_manipulation)
@@ -49,15 +52,21 @@ df$coherence <- as.factor(df$coherence)
 df$cor <- as.factor(df$cor)
 df$cj <- as.factor(df$cj)
 
-### Effect of manipulations on decision RT ###
+
+# Decision RT ----
 
 
-# Based on correct data only
+## Based on correct data only
 
 df_correct <- subset(df, cor == 1)
 df_incorrect <- subset(df, cor == 0)
 
-# Are random slopes necessary? -> Yes (but less for coherence)
+## Log transform
+
+df_correct$rt_log <- log(df_correct$rt)
+df_correct$rt_log <- scale(df_correct$rt_log, scale = FALSE)
+
+## Are random slopes necessary? -> Yes (but less for coherence)
 
 plot1 <- ggplot(df_correct, aes(x = as.factor(rt_manipulation), y = rt, group = sub)) +
   stat_smooth(geom='line', alpha=1, se=FALSE) +
@@ -75,7 +84,7 @@ vplayout <- function(x,y){
   viewport(layout.pos.row=x, layout.pos.col=y)}
 print(plot1, vp=vplayout(1,1)); print(plot2, vp=vplayout(1,2)); print(plot3, vp=vplayout(1,3))
 
-# Model comparisons (through likelihood ratio tests)
+## Model comparisons (through likelihood ratio tests)
 
 RT_1 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), 
              data = df_correct, control = lmerControl(optimizer = "bobyqa"))
@@ -86,9 +95,9 @@ RT_3 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + r
 RT_4 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), 
              data = df_correct, control = lmerControl(optimizer = "bobyqa"))  
 
-anova(RT_1, RT_2)  # Significant; BIC 36388
-anova(RT_1, RT_3)  # Significant; BIC 37541
-anova(RT_1, RT_4)  # Significant; BIC 37470
+anova(RT_1, RT_2)  # Significant; BIC 36263
+anova(RT_1, RT_3)  # Significant; BIC 37418
+anova(RT_1, RT_4)  # Significant; BIC 37342
 
 RT_5 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + coherence|sub), 
              data = df_correct, control = lmerControl(optimizer = "bobyqa"))
@@ -105,25 +114,17 @@ RT_7 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + r
 RT_8 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + coherence * rtconf_manipulation|sub), 
              data = df_correct, control = lmerControl(optimizer = "bobyqa"))  # boundary (singular) fit
 RT_9 <- lmer(rt ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation * rtconf_manipulation + coherence|sub), 
-             data = df_correct, control = lmerControl(optimizer = "bobyqa"))  # boundary (singular) fit
+             data = df_correct, control = lmerControl(optimizer = "bobyqa"))  
 
-# Model assumptions -> violated -> log transform RT
+## Model assumptions
 
-plot(RT_6)
-RT_6_resid <- resid(RT_6)
-RT_6_fit <- fitted(RT_6)
-qqnorm(RT_6_resid)
-qqline(RT_6_resid)
-df_temp <- data.frame(cbind(RT_6_fit, RT_6_resid))
-ggplot(df_temp, aes(x = RT_6_fit, y = RT_6_resid)) + geom_point() + geom_smooth(se = F) + geom_hline(aes(yintercept=0))
+### (1) Linearity
+### Categorical/dummy coded predictors -> assumption met by definition
+### (2) Homogeneity of variance: violated -> log transform RT
+resid_panel(RT_9)
+### (3) Normality assumption: violated
 
-# Log transform
-
-df_correct$rt_log <- log(df_correct$rt)
-
-df_correct$rt_log <- scale(df_correct$rt_log, scale = FALSE) 
-
-# Model selection with log transformed rt
+## Model selection with log transformed rt
 
 RT_7 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), 
              data = df_correct, control = lmerControl(optimizer = "bobyqa"))
@@ -134,14 +135,8 @@ RT_9 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1
 RT_10 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), 
               data = df_correct, control = lmerControl(optimizer = "bobyqa"))  # Boundary (singular) fit
 
-allFit_RT_10 <- allFit(RT_10)
-is.OK <- sapply(allFit_RT_10, is, "merMod")
-allFit_RT_10.OK <- allFit_RT_10[is.OK]
-lapply(allFit_RT_10.OK, function(x) x@optinfo$conv$lme4$messages)
-summary(allFit_RT_10)  # Singular fit for nearly all optimizers
-
-anova(RT_7, RT_8)  # Significant; BIC 24711
-anova(RT_7, RT_9)  # Significant; BIC 25857
+anova(RT_7, RT_8)  # Significant; BIC 23320
+anova(RT_7, RT_9)  # Significant; BIC 24573
 
 RT_11 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation|sub), 
               data = df_correct, control = lmerControl(optimizer = "bobyqa"))
@@ -153,41 +148,48 @@ RT_12 <- lmer(rt_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (
 
 anova(RT_11, RT_12)  # Significant
 
-# Model assumptions
+## Model assumptions
 
-plot(RT_12)
-RT_12_resid <- resid(RT_12)
-RT_12_fit <- fitted(RT_12)
-qqnorm(RT_12_resid)
-qqline(RT_12_resid)
-df_temp <- data.frame(cbind(RT_12_fit, RT_12_resid))
-ggplot(df_temp, aes(x = RT_12_fit, y = RT_12_resid)) + geom_point() + geom_smooth(se = F) + geom_hline(aes(yintercept=0))
+### (1) Linearity
+### Categorical/dummy coded predictors -> assumption met by definition
+### (2) Homogeneity of variance
+resid_panel(RT_12)
+### (3) Normality assumption
 resid_panel(RT_12)
 
-# Multicollinearity - VIF
+### Multicollinearity - VIF
 
 vif.lme(RT_12)
 
-# Model interpretation
+## Model interpretation
 
-Anova(RT_12)
-confint(RT_12)
+Anova(RT_12)  # For p-values
+summary(RT_12)  # For estimates
+confint(RT_12, method = 'boot', parm = 'beta_')
 
 plot(effect('rt_manipulation', RT_12))
-plot(effect('rtconf_manipulation', RT_12))  # Not significant
+plot(effect('rtconf_manipulation', RT_12)) 
 plot(effect('coherence', RT_12))
 plot(effect('rt_manipulation:coherence', RT_12))
 plot(effect('rt_manipulation:rtconf_manipulation', RT_12))
+
+## Further analysis interactions
+
+contrast.matrix <- rbind("rt_manipulation1:rtconf_manipulation1" = c(1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0),
+                         "rt_manipulation1:coherence0.4" = c(1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0),
+                         "rt_manipulation1:coherence0.4" = c(1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0))
+summary(multcomp::glht(RT_12, linfct=contrast.matrix))
 
 #data.frame(effect('rt_manipulation:coherence', RT_12))
 fixef(RT_12)
 ranef(RT_12)
 
 
-### Effect of manipulations on confidence rating RT ###
+
+# Confidence rating RT ----
 
 
-# Are random slopes necessary? 
+## Are random slopes necessary? 
 
 plot1 <- ggplot(df_correct, aes(x = as.factor(rt_manipulation), y = rtconf, group = sub)) +
   stat_smooth(geom='line', alpha=1, se=FALSE) +
@@ -201,19 +203,17 @@ plot3 <- ggplot(df_correct, aes(x = coherence, y = rtconf, group = sub)) +
 
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(1,3)))
-vplayout <- function(x,y){
-  viewport(layout.pos.row=x, layout.pos.col=y)}
 print(plot1, vp=vplayout(1,1))
 print(plot2, vp=vplayout(1,2))
 print(plot3, vp=vplayout(1,3))
 
-# Log transform
+## Log transform
 
 df_correct$rtconf_log <- log(df_correct$rtconf)
 
 df_correct$rtconf_log <- scale(df_correct$rtconf_log, scale = FALSE) 
 
-# Model comparisons (through likelihood ratio tests)
+## Model comparisons (through likelihood ratio tests)
 
 RTconf_1 <- lmer(rtconf_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), 
                  data = df_correct, control = lmerControl(optimizer = "bobyqa"))
@@ -224,14 +224,8 @@ RTconf_3 <- lmer(rtconf_log ~ 1 + rt_manipulation * rtconf_manipulation * cohere
 RTconf_4 <- lmer(rtconf_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), 
                  data = df_correct, control = lmerControl(optimizer = "bobyqa"))  # Boundary (singular) fit
 
-allFit_RTconf_4 <- allFit(RTconf_4)
-is.OK <- sapply(allFit_RTconf_4, is, "merMod")
-allFit_RTconf_4.OK <- allFit_RTconf_4[is.OK]
-lapply(allFit_RTconf_4.OK, function(x) x@optinfo$conv$lme4$messages)
-summary(allFit_RTconf_4)  # Boundary (singular) fit for nearly all optimizers
-
-anova(RTconf_1, RTconf_2)  # Significant; BIC 47547
-anova(RTconf_1, RTconf_3)  # Significant; BIC 44147
+anova(RTconf_1, RTconf_2)  # Significant; BIC 47560
+anova(RTconf_1, RTconf_3)  # Significant; BIC 44066
 
 RTconf_5 <- lmer(rtconf_log ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation|sub), 
                  data = df_correct, control = lmerControl(optimizer = "bobyqa"))
@@ -243,27 +237,22 @@ RTconf_6 <- lmer(rtconf_log ~ 1 + rt_manipulation * rtconf_manipulation * cohere
 
 anova(RTconf_5, RTconf_6) # Significant
 
-# Model assumptions
+## Model assumptions
 
-plot(RTconf_6)
-RTconf_6_resid <- resid(RTconf_6)
-RTconf_6_fit <- fitted(RTconf_6)
-qqnorm(RTconf_6_resid)
-qqline(RTconf_6_resid)
-df_temp <- data.frame(cbind(RTconf_6_fit, RTconf_6_resid))
-ggplot(df_temp, aes(x = RTconf_6_fit, y = RTconf_6_resid)) + geom_point() + geom_smooth(se = F) + geom_hline(aes(yintercept=0))
+### (1) Linearity
+### Categorical/dummy coded predictors -> assumption met by definition
+### (2) Homogeneity of variance: assumption not met -> log transform
+resid_panel(RTconf_6)
+### (3) Normality (of errors) assumption
 resid_panel(RTconf_6)
 
-# Multicollinearity - VIF
-
-vif.lme(RTconf_6)
-
-# Model interpretation
+## Model interpretation
 
 Anova(RTconf_6)
-confint(RTconf_6)
+summary(RTconf_6)
+confint(RTconf_6, method = 'boot', parm = 'beta_')
 
-plot(effect('rt_manipulation', RTconf_6))  # Not significant
+plot(effect('rt_manipulation', RTconf_6))  
 plot(effect('rtconf_manipulation', RTconf_6)) 
 plot(effect('coherence', RTconf_6))
 plot(effect('rt_manipulation:rtconf_manipulation', RTconf_6))
@@ -273,10 +262,10 @@ fixef(RTconf_6)
 ranef(RTconf_6)
 
 
-### Effect of manipulations on accuracy ###
+# Accuracy ----
 
 
-# Are random slopes necessary? 
+## Are random slopes necessary? 
 
 plot1 <- ggplot(df, aes(x = as.factor(rt_manipulation), y = cor, group = sub)) +
   stat_smooth(geom='line', alpha=1, se=FALSE) +
@@ -290,13 +279,11 @@ plot3 <- ggplot(df, aes(x = coherence, y = cor, group = sub)) +
 
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(1,3)))
-vplayout <- function(x,y){
-  viewport(layout.pos.row=x, layout.pos.col=y)}
 print(plot1, vp=vplayout(1,1))
 print(plot2, vp=vplayout(1,2))
 print(plot3, vp=vplayout(1,3))
 
-# Model comparisons (through likelihood ratio tests)
+## Model comparisons (through likelihood ratio tests)
 
 Cor_1 <- glmer(cor ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), 
                data = df, family = binomial, control = glmerControl(optimizer = "bobyqa"))
@@ -305,28 +292,22 @@ Cor_2 <- glmer(cor ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 
 Cor_3 <- glmer(cor ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rtconf_manipulation|sub), 
                data = df, family = binomial, control = glmerControl(optimizer = "bobyqa"))  
 Cor_4 <- glmer(cor ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), 
-               data = df, family = binomial, control = glmerControl(optimizer = "bobyqa"))  # Boundary (singular) fit
+               data = df, family = binomial, control = glmerControl(optimizer = "bobyqa"))  
 
-allFit_Cor_4 <- allFit(Cor_4)
-is.OK <- sapply(allFit_Cor_4, is, "merMod")
-allFit_Cor_4.OK <- allFit_Cor_4[is.OK]
-lapply(allFit_Cor_4.OK, function(x) x@optinfo$conv$lme4$messages)
-summary(allFit_Cor_4)  # Boundary (singular) fit for nearly all optimizers
-
-anova(Cor_1, Cor_2)  # Significant
+anova(Cor_1, Cor_2)  # Significant; BIC 32237
 anova(Cor_1, Cor_3)  # Not significant
+anova(Cor_1, Cor_4)  # Significant; BIC 32078 (additionally: lower AIC, higher logLik than Cor_2)
 
-Cor_5 <- glmer(cor ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + rtconf_manipulation|sub), 
-                  data = df, family = binomial, control = glmerControl(optimizer = "bobyqa"))  # Fails to converge
+Cor_5 <- glmer(cor ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + coherence|sub), 
+                  data = df, family = binomial, control = glmerControl(optimizer = "bobyqa"))  # Boundary (singular) fit
 
-anova(Cor_2, Cor_5)  # Not significant
+## Model assumptions
 
-# Model assumptions
-
-# (1) Independence of errors
+### (1) Independence of errors
 # Gray lines indicate plus and minus 2 standard-error bounds (around 95% of residuals)
-binnedplot(fitted(Cor_2), 
-           residuals(Cor_2, type = "response"), 
+
+binnedplot(fitted(Cor_4), 
+           residuals(Cor_4, type = "response"), 
            nclass = NULL, 
            xlab = "Expected Values", 
            ylab = "Average residual", 
@@ -335,38 +316,23 @@ binnedplot(fitted(Cor_2),
            col.pts = 1, 
            col.int = "gray")
 
-plot(Cor_2)
-plot(predict(Cor_2), residuals(Cor_2), col = c("blue","red")[as.numeric(df$cor)])
-abline(h=0,lty=2,col="grey")
-lines(lowess(predict(Cor_2), residuals(Cor_2)), col="black", lwd=2)
+### (2) Linearity (no continuous variables)
 
-rl=lm(residuals(Cor_2)~bs(predict(Cor_2),8))
-y=predict(rl,se=TRUE)
-segments(predict(Cor_2),y$fit+2*y$se.fit,predict(Cor_2),y$fit-2*y$se.fit,col="green")
+### (3) Absence of multicollinearity
 
-# (2) Linearity (no continuous variables)
+vif.lme(Cor_4)
 
-# (3) Absence of multicollinearity
+## Model interpretation
 
-vif.lme(Cor_2)
-
-# Model interpretation
-
-Anova(Cor_2)
-
-plot(effect('rt_manipulation', Cor_2))  # Not significant
-plot(effect('rtconf_manipulation', Cor_2))   # Not significant
-plot(effect('coherence', Cor_2))
-
-#data.frame(effect('rt_manipulation:coherence', RTconf_6))
-fixef(RTconf_6)
-ranef(RTconf_6)
+Anova(Cor_4)
+summary(Cor_4)
+confint(Cor_4, method = 'boot', parm = 'beta_')
 
 
-### Effect of manipulations on confidence judgements ###
+# Confidence judgements ----
 
 
-# Are random slopes necessary? 
+## Are random slopes necessary? 
 
 plot1 <- ggplot(df_correct, aes(x = as.factor(rt_manipulation), y = cj, group = sub)) +
   stat_smooth(geom='line', alpha=1, se=FALSE) +
@@ -380,13 +346,11 @@ plot3 <- ggplot(df_correct, aes(x = coherence, y = cj, group = sub)) +
 
 grid.newpage()
 pushViewport(viewport(layout=grid.layout(1,3)))
-vplayout <- function(x,y){
-  viewport(layout.pos.row=x, layout.pos.col=y)}
 print(plot1, vp=vplayout(1,1))
 print(plot2, vp=vplayout(1,2))
 print(plot3, vp=vplayout(1,3))
 
-# Model comparisons (through likelihood ratio tests)
+## Model comparisons (through likelihood ratio tests)
 
 cj_1 <- glmer(cj ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1|sub), 
                data = df_correct, family = binomial, control = glmerControl(optimizer = "bobyqa"))
@@ -397,9 +361,9 @@ cj_3 <- glmer(cj ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + 
 cj_4 <- glmer(cj ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence|sub), 
                data = df_correct, family = binomial, control = glmerControl(optimizer = "bobyqa"))  
 
-anova(cj_1, cj_2)  # Significant; BIC 24298
-anova(cj_1, cj_3)  # Significant; BIC 24335
-anova(cj_1, cj_4)  # Significant; BIC 24171
+anova(cj_1, cj_2)  # Significant; BIC 24211
+anova(cj_1, cj_3)  # Significant; BIC 24248
+anova(cj_1, cj_4)  # Significant; BIC 24089
 
 cj_5 <- glmer(cj ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence + rt_manipulation|sub), 
                data = df_correct, family = binomial, control = glmerControl(optimizer = "bobyqa"))
@@ -414,16 +378,14 @@ anova(cj_5, cj_6)  # Significant
 cj_7 <- glmer(cj ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence * rt_manipulation + rtconf_manipulation|sub), 
               data = df_correct, family = binomial, control = glmerControl(optimizer = "bobyqa"))  # Boundary (singular) fit
 cj_8 <- glmer(cj ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence + rt_manipulation * rtconf_manipulation|sub), 
-              data = df_correct, family = binomial, control = glmerControl(optimizer = "bobyqa"))  
+              data = df_correct, family = binomial, control = glmerControl(optimizer = "bobyqa"))  # Boundary (singular) fit
 cj_9 <- glmer(cj ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + coherence * rtconf_manipulation + rt_manipulation|sub), 
               data = df_correct, family = binomial, control = glmerControl(optimizer = "bobyqa"))  # Boundary (singular) fit
 
-anova(cj_6, cj_8)  # Not significant
+## Model assumptions
 
-# Model assumptions
-
-# (1) Independence of errors
-# Gray lines indicate plus and minus 2 standard-error bounds (around 95% of residuals)
+### (1) Independence of errors
+### Gray lines indicate plus and minus 2 standard-error bounds (around 95% of residuals)
 binnedplot(fitted(cj_6), 
            residuals(cj_6, type = "response"), 
            nclass = NULL, 
@@ -434,40 +396,24 @@ binnedplot(fitted(cj_6),
            col.pts = 1, 
            col.int = "gray")
 
-plot(cj_6)
-plot(predict(cj_6), residuals(cj_6), col = c("blue","red")[as.numeric(df$cor)])
-abline(h=0,lty=2,col="grey")
-lines(lowess(predict(cj_6), residuals(cj_6)), col="black", lwd=2)
+### (2) Linearity (no continuous variables)
 
-rl=lm(residuals(cj_6)~bs(predict(cj_6),8))
-y=predict(rl,se=TRUE)
-segments(predict(cj_6),y$fit+2*y$se.fit,predict(cj_6),y$fit-2*y$se.fit,col="green")
-
-# (2) Linearity (no continuous variables)
-
-# (3) Absence of multicollinearity
+### (3) Absence of multicollinearity
 
 vif.lme(cj_6)
 
-# Model interpretation
+## Model interpretation
 
 Anova(cj_6)
+summary(cj_6)
+confint(cj_6, method = 'boot', parm = 'beta_')
 
-plot(effect('rt_manipulation', cj_8))  # Not significant
-plot(effect('rtconf_manipulation', cj_8))   # Not significant
+plot(effect('rt_manipulation', cj_8)) 
+plot(effect('rtconf_manipulation', cj_8))   
 plot(effect('coherence', cj_8))
 plot(effect('rt_manipulation:coherence', cj_8))
 
-#data.frame(effect('rt_manipulation:coherence', RTconf_6))
-fixef(RTconf_6)
-ranef(RTconf_6)
 
-
-
-
-cj_9 <- glmer(cj ~ 1 + rt_manipulation * rtconf_manipulation * coherence + (1 + rt_manipulation + coherence|sub), 
-              data = df_incorrect, family = binomial, control = glmerControl(optimizer = "bobyqa"))  # Boundary (singular) fit
-Anova(cj_9)
 
 
 
